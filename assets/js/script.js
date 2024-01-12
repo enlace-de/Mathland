62cafa2824b2f215306d2b6e41aa0704e6d2a89a
enlace-de <prisypau81@gmail.com>
2024-01-12 17:14:05 -0400

    3

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,9 +90,7 @@ logoImage.addEventListener('click', () => {
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
-  // Stash the event so it can be triggered later.
   deferredPrompt = e;
 });
 
