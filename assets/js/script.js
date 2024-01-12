5549829c55a2004c7cabca8e4ad10e1b5ce9d485
enlace-de <prisypau81@gmail.com>
2024-01-12 16:53:18 -0400

    btn aplicacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,6 +85,33 @@ logoImage.addEventListener('click', () => {
     logoImage.classList.add('animacion');
 });
 
+// funcion agregar a pagina de inicio
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+function addToHomeScreen() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  } else {
+    alert("Para agregar a la pantalla de inicio, utiliza la opción del navegador.");
+  }
+}
+
 
 
 
